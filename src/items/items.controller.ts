2e88d06b2bb86a2dcaa6cdc323f5d4f22d958f12
MuhireIghor <muhireighor123@gmail.com>
2022-12-28 22:16:18 +0200

    [updates]:Item controller  updates

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,7 +1,10 @@
-import { Controller,Post,Get,Put,Delete,Param,Body } from '@nestjs/common';
+import { Controller,Post,Get,Put,Delete,Param,Body, Req, Query, Res, NotFoundException } from '@nestjs/common';
 import { CreateItemDto } from './dto/createItem.dto';
 import { ItemsService } from './items.service';
+import  {Pricedto} from './dto/price.dto'
 import { Item } from './interfaces/items.interface';
+import { query } from 'express';
+import { log } from 'console';
 @Controller('items')
 export class ItemsController {
   constructor(private readonly itemService:ItemsService){}
@@ -9,10 +12,29 @@ export class ItemsController {
   getAll():Promise<Item[]>{
     return  this.itemService.findAll();
   }
+  @Get('price')
+  getByPrice(@Query() query:{price:string},@Res() res:Response):Promise<Item[]|Item >|any{
+    const price2 = Number(query.price) ;
+    try{
+  return this.itemService.getItemByPrice(price2).then(val=>{
+    if(val.length ==0){
+  console.log("No items found");
+    
+    }
+  });
+
+    }
+    catch(err){
+      return new NotFoundException("Object not found","no item with the required price");
+
+    }
+  
+  }
   @Get(':id')
     getById(@Param('id') id):Promise<Item>{  
     return  this.itemService.findOne(id);
   }
+
   @Post()
   createItem(@Body() createItemDto:CreateItemDto):Promise<Item>{
       return this.itemService.create(createItemDto);
