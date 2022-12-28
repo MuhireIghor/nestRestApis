10637244a4435b2c8261bc76534ddf78f8d25aa5
MuhireIghor <muhireighor123@gmail.com>
2022-12-28 22:16:55 +0200

    [updates]:Items service  updates

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -7,22 +7,27 @@ import { InjectModel } from '@nestjs/mongoose';
 @Injectable()
 
 export class ItemsService {
-    constructor(@InjectModel('Item') private readonly itemModel:Model<Item>){}
-    async findAll():Promise<Item[]>{
+    constructor(@InjectModel('Item') private readonly itemModel: Model<Item>) { }
+    async findAll(): Promise<Item[]> {
         return await this.itemModel.find();
     }
-    async findOne(id:string):Promise<Item>{
-        return await this.itemModel.findOne({_id:id});
+    async findOne(id: string): Promise<Item> {
+        return await this.itemModel.findOne({ _id: id });
     }
-    async create(item:Item):Promise<Item>{
+    async create(item: Item): Promise<Item> {
         const newItem = new this.itemModel(item);;
         return await newItem.save()
     }
-    async deleteItem(id:string):Promise<Item>{
+    async deleteItem(id: string): Promise<Item> {
         return await this.itemModel.findByIdAndRemove(id);
     }
-    async updateItem(id:string,item:Item):Promise<Item>{
-        return await this.itemModel.findByIdAndUpdate(id,item,{new:true})
+    async updateItem(id: string, item: Item): Promise<Item> {
+        return await this.itemModel.findByIdAndUpdate(id, item, { new: true })
+    }
+    async getItemByPrice(price: number): Promise<Item[]> {
+        const items = await this.itemModel.find({ price: price });
+        return await this.itemModel.find({ price: price });
+
     }
 
 }
